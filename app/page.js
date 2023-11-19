a9b2ccd74224c46c8d81765adc4d724978790d6a
Revanth Sai <revanth.dontamsetty1999@gmail.com>
2023-11-19 22:41:28 +0530

    Fixed root path

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <p>Welcome to Denest</p>;
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/hostels");
+  else redirect("/auth/login");
 }
